62df8f2c1cdc761cc49b29e58afb7b4f3d6c1d39
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2016-07-29 17:23:59 +0800

    start work on archive

---
+++ themes/meilidu/layout/_widgets/greeting.js
@@ -0,0 +1,3 @@
+<div class="footer__element">
+	<p>Hi there, <br />welcome to my Blog glad you found it. Have a look around, will you?</p>
+</div>
---
+++ themes/meilidu/layout/_widgets/site_links.js
@@ -0,0 +1,7 @@
+<div class="footer__element">
+	<ul class="footer-links">
+		<li class="footer-links__link"><a href="/archives">Archive</a></li>
+		<li class="footer-links__link"><a href="#">The author</a></li>
+		<li class="footer-links__link"><a href="#">more links</a></a></li>
+	</ul>
+</div>
