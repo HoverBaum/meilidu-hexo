f60f18e3d48a4f2bf5b28b7ea9376ae993754750
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2016-08-10 15:20:51 +0800

    :art: Image captions and remove fancybox

---
+++ themes/meilidu/scripts/imageCaption.js
@@ -0,0 +1,14 @@
+/*
+	Big thanks to https://github.com/wayou/hexo-image-caption
+ */
+
+hexo.extend.filter.register('after_post_render', function(data) {
+
+        var className = 'figure__caption';
+        if (data.layout == 'post' || data.layout == 'page' || data.layout == 'about') {
+			data.content = data.content.replace(/(<img [^>]*>)/g, '<figure class="figure">$1</figure>')
+            data.content = data.content.replace(/(<img [^>]*alt="([^"]+)"[^>]*>)/g, '$1' + '<figcaption class="figure__caption">$2</figcaption>');
+        }
+
+    return data;
+});
