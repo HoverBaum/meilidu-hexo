7b6240722d107bf78b764fbb8b64fd4b202f6d7f
Hendrik Wallbaum <mail@hendrikwallbaum.de>
2017-04-09 15:12:10 +0200

    :sparkles: add script to check node version

---
+++ scripts/meilidu.js
@@ -0,0 +1,13 @@
+/*
+	A little script to make sure that the node version is high enough as we
+	had multiple issues of people running a not high enough version.
+
+ */
+
+if(process.versions.node < '6') {
+	console.log('Your Node version is too old to use MeiliDu.')
+	console.log(`MeiliDu needs at least version 6 you are running ${process.versions.node}`)
+	console.log('\nUse "npm install -g n" to update your Node version.')
+	console.log('\nYou might find the following link informative:\nhttps://github.com/HoverBaum/meilidu-hexo/issues/3')
+	process.exit(1)
+}
